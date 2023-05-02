39af02419ff0fb5e1797a5c76719c8ba7b2944de
Sean Nance <sean_nance@hotmail.com>
2023-05-02 14:57:54 -0700

    modified script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-function arrayFilter(sentence){
+function arrayFilter(sentence) {
     let wordsInSentence = sentence.split(" ");
-    console.log (wordsInSentence)
-    var chars = sentence.length;
-    console.log(chars);
+    let output = "";
     let wordsArray = [];
-    console.log (wordsArray);
-    for (var i=0; i<wordsInSentence.length;i++){
-        console.log(wordsInSentence[i]);
-        if (string.wordsInSentence[i].length > 3) {
-            outputString += wordsInSentence[i] + " ";
-            outputString.push
+    for (var i = 0; i < wordsInSentence.length; i++) {
+        if (wordsInSentence[i].length > 3) {
+            output += wordsInSentence[i] + " ";
+            wordsArray.push(output);
         }
-            
     }
+    console.log(wordsArray.join(" "));
+    
+    return wordsArray.join(" ");
 }
 
-arrayFilter("London is a big city in the United Kingdom")
-// should return "London city United Kingdom"
+arrayFilter("London is a big city in the United Kingdom");
