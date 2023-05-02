5749ab652c9fdadbd01b581fd7b4c4319887df80
Sean Nance <sean_nance@hotmail.com>
2023-05-02 12:59:06 -0700

    modified script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function arrayFilter(sentence){
-    // FUNCTION CODE GOES HERE
+    let wordsInSentence = sentence.split(" ");
+    console.log (wordsInSentence)
+    var chars = sentence.length;
+    console.log(chars);
+    let wordsArray = [];
+    console.log (wordsArray);
+    for (var i=0; i<wordsInSentence.length;i++){
+        console.log(wordsInSentence[i]);
+        if (string.wordsInSentence[i].length > 3) {
+            outputString += wordsInSentence[i] + " ";
+            outputString.push
+        }
+            
+    }
 }
 
 arrayFilter("London is a big city in the United Kingdom")
 // should return "London city United Kingdom"
-
